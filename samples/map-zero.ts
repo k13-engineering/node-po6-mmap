d167451d31b26f9b00acd1f191bd687e5a6159a6
Simon Kadisch <simon.kadisch@k13-engineering.at>
2025-12-31 13:51:55 +0100

    Add sample how to map /dev/zero

---
+++ samples/map-zero.ts
@@ -0,0 +1,28 @@
+import { mmapFd, determinePageSize } from "../lib/index.ts";
+import nodeFs from "node:fs";
+
+const fd = nodeFs.openSync("/dev/zero", "r+");
+
+const length = determinePageSize();
+
+const { errno, buffer } = mmapFd({
+  fd,
+  mappingVisibility: "MAP_PRIVATE",
+  memoryProtectionFlags: {
+    PROT_READ: true,
+    PROT_WRITE: false,
+    PROT_EXEC: false,
+  },
+  genericFlags: {},
+  offsetInFd: 0,
+  length,
+});
+
+if (errno !== undefined) {
+  throw Error(`mmapFd failed with errno ${errno}`);
+}
+
+console.log(`mapped buffer of length ${buffer.length} at address 0x${buffer.address.toString(16)}`);
+console.log(`buffer:`, buffer);
+
+buffer.unmap();
